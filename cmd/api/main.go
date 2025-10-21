5ac52b6b1bd951207694281d7a97a1571f6de76f
Enzzo Hosaki Maciel <enzzohosaki@hotmail.com>
2025-10-21 16:45:46 -0300

    feat: estrutura o repositório do projeto

---
+++ cmd/api/main.go
@@ -0,0 +1,8 @@
+// Local: rps-maestro/cmd/api/main.go
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Iniciando o backend do RPS Maestro...")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/EnzzoHosaki/rps-maestro
+
+go 1.22.2
